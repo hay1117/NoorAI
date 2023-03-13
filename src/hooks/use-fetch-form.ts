9186a68dbd0bea5468eb3e6e278205f62e8dd717
hay1117 <kurainakamura047@gmail.com>
2023-03-13 13:18:11 +0100

    feat: implement the hooks

---
+++ src/hooks/use-fetch-form.ts
@@ -0,0 +1,125 @@
+import { useForm } from "react-hook-form";
+import { useStore } from ".";
+import { useRouter } from "next/router";
+import {
+  type ChatCompletionRequestMessage,
+  type CreateChatCompletionRequest,
+  type CreateChatCompletionResponse,
+} from "openai";
+import { useMutation } from "@tanstack/react-query";
+
+interface FormData {
+  promptText: string;
+}
+type OpenaiFetcherParams = {
+  apiKey: string;
+  body: CreateChatCompletionRequest;
+};
+
+const openaiFetcher = ({
+  apiKey,
+  body,
+}: OpenaiFetcherParams): Promise<CreateChatCompletionResponse> => {
+  const customBody = {
+    max_tokens: 300,
+    ...body,
+  };
+  return fetch("https://api.openai.com/v1/chat/completions", {
+    method: "post",
+    headers: {
+      authorization: `Bearer ${apiKey}`,
+      "Content-Type": "application/json",
+    },
+    body: JSON.stringify(customBody),
+  })
+    .then((r) => r.json())
+    .catch((e) => console.error(e));
+};
+
+export const useFetchForm = () => {
+  const methods = useForm<FormData>();
+
+  const { query } = useRouter();
+  const push = useStore((s) => s.push);
+  const apiKey = useStore((s) => s.apiKey);
+  const { getValues } = methods;
+  const conversationId = query.chatId as string;
+  // disabled/ //------------------------------INTERNAL API
+  // const trpcRes = api.openai.simple.useMutation({
+  //   onSuccess: (d) => {
+  // // save data in store, and update status
+  // // @ts-expect-error when request is not correct
+  //     if (!d.error) {
+  //       push(conversationId, d as ChatPairT);
+  //     }
+  //   },
+  // });
+  //------------------------------OpenAI
+  const openAPI = useMutation(
+    ["openai"],
+    (d: OpenaiFetcherParams) => openaiFetcher(d),
+    {
+      onSuccess: (d) => {
+        if (d?.choices) {
+          updateStatus("success");
+          push(conversationId, {
+            input: getValues("promptText"),
+            message: d.choices.reduce(
+              (acc, { message }) => ({
+                ...acc,
+                role: message?.role,
+                content: message?.content,
+              }),
+              {}
+            ) as ChatCompletionRequestMessage,
+          });
+        } else {
+          console.warn(d);
+        }
+        window.scrollTo({
+          top: document.body.scrollHeight,
+          behavior: "smooth",
+        });
+      },
+      onError: () => {
+        updateStatus("error");
+      },
+    }
+  );
+  const updateStatus = useStore((s) => s.updateStatus);
+
+  const conversation = useStore((s) => s.conversations).find(
+    (o) => o.id === conversationId
+  );
+
+  // const { mutate } = res;
+  const onSubmit = async ({ promptText: input }: FormData) => {
+    if (apiKey) {
+      updateStatus("loading");
+      // get and store previous messages
+      openAPI.mutate({
+        apiKey,
+        body: {
+          messages: [
+            ...(conversation?.thread.map((o) => ({
+              content: o.input,
+              role: o?.message?.role || "user",
+            })) || []),
+            {
+              role: "user",
+              content: input.trim(),
+            },
+          ],
+          model: "gpt-3.5-turbo-0301",
+        },
+      });
+    }
+    window.scrollTo({
+      top: document.body.scrollHeight,
+      behavior: "smooth",
+    });
+    // save status in store
+    // mutate({ promptText: input.trim() });
+  };
+  return { methods, onSubmit };
+};
---
+++ src/hooks/use-store.ts
@@ -0,0 +1,243 @@
+import { create } from "zustand";
+import { persist, devtools, createJSONStorage } from "zustand/middleware";
+import type {
+  ChatCompletionResponseMessage,
+  CreateCompletionResponseChoicesInner,
+} from "openai";
+import { type FetchStatus, type QueryStatus } from "@tanstack/react-query";
+
+/**
+ **Request
+ * [{role,content(prompt)}]
+ *
+ **Response
+ * [{message:{content:string, role}}]
+ *
+ **Store
+ * thread: [{input,content,role}]
+ *
+ **UI
+ *
+ *
+ */
+
+export interface ChatPairT {
+  /**
+   * @description user prompt
+   *
+   */
+  input: string;
+  /**
+   * @deprecated replace with message
+   *
+   */
+  response?: CreateCompletionResponseChoicesInner[];
+  /**
+   * @description
+   * content string,
+   * role "user"| "assistent" | "system"
+   */
+  message?: ChatCompletionResponseMessage;
+}
+
+export interface ConversationT {
+  id: string | string;
+  name?: string;
+  createdAt: number;
+  thread: ChatPairT[];
+}
+
+export type ConversationsT = ConversationT[];
+
+export interface StoreStateT {
+  id: number;
+  filtered: ConversationsT | never[];
+  conversations: ConversationsT;
+  apiKey: string;
+  /**
+   * @required
+   */
+  status: QueryStatus | FetchStatus;
+  updateStatus: (s: QueryStatus | FetchStatus) => void;
+
+  saveApiKey: (apiKey: string) => void;
+  /**
+   * @required push
+   *
+   * @params id: conversation id from router
+   * @param chatPair { input: string, response: {text:string}[] }
+   */
+  push: (id: number | string, chatPair: ChatPairT) => void;
+  /**
+   * @required
+   * create an empty conversation when creating a new chat (new chat button)
+   */
+  createConversation: (id: string) => void;
+  /**
+   * @required
+   */
+  deleteConversation: (id: string) => void;
+  /**
+   * @required search for prompts in conversations
+   *
+   */
+  //  search: () => void;
+  renameConversation: (id: string, name: string) => void;
+  /**
+   * @required filter prompts
+   */
+  filter: (input: string) => void;
+  /**
+   * @required
+   *
+   */
+  delChatPair: (index: number, conversationId: string) => void;
+}
+
+const localStorageKey = "yourchatgpt";
+
+export const useStore = create<StoreStateT>()(
+  devtools(
+    persist(
+      (set) => {
+        return {
+          id: new Date().getTime(),
+          status: "idle",
+          apiKey: "",
+          filtered: [],
+          conversations: [
+            {
+              id: "chat",
+              name: "First Chat",
+              thread: [],
+              createdAt: new Date().getTime(),
+            },
+          ],
+          push: (id, chatPair) => {
+            return set(
+              (state) => {
+                const { conversations } = state;
+
+                const index = state.conversations.findIndex((o) => o.id === id);
+
+                if (index >= -1) {
+                  conversations[index]?.thread.push(chatPair);
+                } else {
+                  console.warn("push action: index is not found", {
+                    index,
+                    conversationId: id,
+                  });
+                }
+
+                return {
+                  ...state,
+                  conversations,
+                } as StoreStateT;
+              },
+              false,
+              "push"
+            );
+          },
+          createConversation: (id) =>
+            set(
+              (s) =>
+                ({
+                  conversations: [
+                    ...s.conversations,
+                    {
+                      id,
+                      created: new Date().getTime(),
+                      thread: [],
+                    },
+                  ],
+                } as StoreStateT),
+              false,
+              "createConversation"
+            ),
+          deleteConversation: (id) =>
+            set(
+              (s) => {
+                const { conversations } = s;
+                const index = conversations.findIndex((o) => o.id === id);
+                if (index > -1) {
+                  conversations.splice(index, 1);
+                }
+                return { ...s };
+              },
+              false,
+              "deleteConversation"
+            ),
+          renameConversation: (id, name) =>
+            set(
+              (s) => {
+                const { conversations } = s;
+                const index = conversations.findIndex((o) => o.id === id);
+                if (index > -1) {
+                  const conversation = {
+                    ...conversations[index],
+                    name,
+                  } as ConversationT;
+                  console.log(index);
+                  conversations.splice(index, 1, conversation);
+                }
+                return { conversations };
+              },
+              false,
+              "renameConversation"
+            ),
+          filter: (input) =>
+            set((s) => {
+              const { conversations } = s;
+              const filtered = [];
+              for (const con of conversations) {
+                if (con.name?.includes(input)) {
+                  filtered.push(con);
+                } else {
+                  const hasInput = con.thread.some((chatPair) =>
+                    chatPair.input.toLowerCase().includes(input.toLowerCase())
+                  );
+                  if (hasInput) {
+                    filtered.push(con);
+                  }
+                }
+              }
+              return { filtered };
+            }),
+          saveApiKey: (apiKey) => set({ apiKey }),
+          updateStatus: (status) => set(() => ({ status: status })),
+          delChatPair: (index, conversationId) =>
+            set((s) => {
+              const { conversations } = s;
+              const conversation = conversations.find(
+                (o) => o.id === conversationId
+              );
+              conversation?.thread.splice(index, 1);
+              return {
+                ...s,
+              };
+            }),
+        };
+      },
+      { name: localStorageKey, storage: createJSONStorage(() => localStorage) }
+    ),
+    { enabled: false }
+  )
+);
+
+// type setT = <
+//   A extends
+//     | string
+//     | {
+//         type: unknown;
+//       }
+// >(
+//   partial:
+//     | StoreStateT
+//     | Partial<StoreStateT>
+//     | ((state: StoreStateT) => StoreStateT | Partial<StoreStateT>),
+//   replace?: boolean | undefined,
+//   action?: A | undefined
+// ) => void;
+
+// const middlewares = (f) =>
+//   devtools(persist(f, { name: localStorageKey }), { enabled: false });
