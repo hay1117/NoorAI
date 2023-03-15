9849b9114ca24a2b3b9e6cef77584ab0189fc4a0
hay1117 <kurainakamura047@gmail.com>
2023-03-15 08:19:50 +0100

    feat: integration Notion API

--- src/server/api/root.ts
+++ src/server/api/root.ts
@@ -1,5 +1,5 @@
 import { createTRPCRouter } from "~/server/api/trpc";
-import { exampleRouter } from "~/server/api/routers/example";
+import { promptsRouter } from "~/server/api/routers/prompts";
 
 /**
  * This is the primary router for your server.
@@ -7,7 +7,7 @@ import { exampleRouter } from "~/server/api/routers/example";
  * All routers added in /api/routers should be manually added here.
  */
 export const appRouter = createTRPCRouter({
-  example: exampleRouter,
+  prompts: promptsRouter,
 });
 
 // export type definition of API
