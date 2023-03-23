3d377cf519d016200f0aaf02fbcf38207721b09a
hay1117 <kurainakamura047@gmail.com>
2023-03-23 15:25:18 +0100

    feat: tweak the sign in page a bit

--- src/pages/signin.tsx
+++ src/pages/signin.tsx
@@ -1,4 +1,4 @@
-import { Button, Title, Paper } from "@mantine/core";
+import { Button, Title, Paper, Divider } from "@mantine/core";
 import { type GetServerSidePropsContext } from "next";
 import { getProviders, getSession, signIn } from "next-auth/react";
 
@@ -11,29 +11,37 @@ const Signin = ({
   providers: Awaited<ReturnType<typeof getProviders>>;
 }) => {
   return (
-    <div className="grid min-h-screen place-items-center">
-      <Paper radius="lg" withBorder shadow="lg" p="xl" className="px-20">
-        <Title order={2} ta="center">
-          Log in
-        </Title>
-        <React.Suspense fallback="loading...">
-          <div className="gap-y-4 pt-8 flex-col-center">
-            {providers &&
-              Object.values(providers).map((prov) => (
-                <Button
-                  key={prov.name}
-                  variant="outline"
-                  color="gray"
-                  size="lg"
-                  radius="lg"
-                  leftIcon={prov.id === "github" ? <BsGithub /> : <BsGoogle />}
-                  onClick={() => void signIn(prov.id)}
-                >
-                  {prov.name}
-                </Button>
-              ))}
-          </div>
-        </React.Suspense>
+    <div className="grid min-h-screen place-items-center px-4">
+      <Paper
+        radius="lg"
+        shadow="lg"
+        p="lg"
+        withBorder
+        className="max-w-lg px-4 "
+        w="100%"
+      >
+        <div className="w-full">
+          <Title order={2} ta="center">
+            Sign up/ Log in
+          </Title>
+          <Divider w="100%" />
+        </div>
+        <div className="gap-y-4 pt-8 flex-col-center">
+          {providers &&
+            Object.values(providers).map((prov) => (
+              <Button
+                key={prov.name}
+                variant="outline"
+                color="gray"
+                size="lg"
+                radius="md"
+                leftIcon={prov.id === "github" ? <BsGithub /> : <BsGoogle />}
+                onClick={() => void signIn(prov.id)}
+              >
+                {prov.name}
+              </Button>
+            ))}
+        </div>
       </Paper>
     </div>
   );
