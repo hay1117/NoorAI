c0945697a9694f6ac11a9ce33b1359bdb2184f77
hay1117 <kurainakamura047@gmail.com>
2023-04-26 17:18:34 +0200

    feat: whats new for initial view

--- src/components/Chats.tsx
+++ src/components/Chats.tsx
@@ -5,173 +5,33 @@ import {
   useFetchForm,
 } from "~/hooks";
 import { useRouter } from "next/router";
-import {
-  BsFillEmojiFrownFill,
-  BsFillEmojiSmileFill,
-  BsStopFill,
-} from "react-icons/bs";
+import { BsStopFill } from "react-icons/bs";
 import ReactMarkdown from "react-markdown";
-import { MdDelete, MdInfo, MdOutlineContentCopy } from "react-icons/md";
+import { MdDelete, MdOutlineContentCopy } from "react-icons/md";
 import { TbReload } from "react-icons/tb";
 import { type Language } from "prism-react-renderer";
 import {
-  Title,
   ActionIcon,
   Avatar,
   Text,
   Paper,
-  Drawer,
-  Card,
-  Divider,
   Tooltip,
   useMantineTheme,
-  Badge,
-  Spoiler,
   Button,
   Textarea,
+  Spoiler,
 } from "@mantine/core";
 import remarkGfm from "remark-gfm";
-import { useDisclosure, useClipboard } from "@mantine/hooks";
+import { useClipboard } from "@mantine/hooks";
 import * as React from "react";
-import clsx from "clsx";
-import { HiArrowSmRight } from "react-icons/hi";
 import dynamic from "next/dynamic";
-import promptTips from "~/content/prompt-tips.json";
-import verbs from "~/content/verbs.json";
-import tones from "~/content/tones.json";
 import { FiEdit3 } from "react-icons/fi";
 import { franc } from "franc";
+import { InitialChatsView } from "~/components";
 
 const Prism = dynamic(() => import("@mantine/prism").then((c) => c.Prism), {
   ssr: false,
 });
-
-//======================================
-export const PromptTips = () => {
-  const [opened, { open, close }] = useDisclosure(false);
-  const [drawerChildren, setdrawerChildren] = React.useState(<div></div>);
-  return (
-    <>
-      <div className="prose max-w-full">
-        <Title order={2} className="gap-2 flex-row-center">
-          <MdInfo size="24" />
-          Prompt Tips & Tricks
-        </Title>
-        <Drawer
-          opened={opened}
-          onClose={close}
-          title={""}
-          overlayProps={{
-            opacity: 0.2,
-          }}
-          position="right"
-          size="sm"
-        >
-          {drawerChildren}
-        </Drawer>
-        <div className="grid grid-cols-1 gap-2 sm:grid-cols-2 lg:grid-cols-6">
-          {promptTips.map((o, i) => (
-            <div
-              key={i}
-              className={clsx(
-                i < 3 && "lg:col-span-2",
-                i == 3 && "lg:col-span-3",
-                i == 4 && "col-span-full lg:col-span-3"
-              )}
-            >
-              <Card
-                component="button"
-                onClick={() => {
-                  open();
-                  setdrawerChildren(
-                    <div className="space-y-3">
-                      <Title order={4} color="dimmed">
-                        {o.title}
-                      </Title>
-                      <Text size="lg">{o.description}</Text>
-                      <Divider />
-                      <Title order={5}>Examples:</Title>
-                      <div className="flex flex-col items-start gap-y-2">
-                        <BsFillEmojiFrownFill size="28" />
-                        <Text size="lg" color="dimmed">
-                          {o.b}
-                        </Text>
-                      </div>
-                      <div className="flex flex-col items-start gap-y-2">
-                        <BsFillEmojiSmileFill size="28" />
-                        <Text size="lg" color="dimmed">
-                          {o.g}
-                        </Text>
-                      </div>
-                    </div>
-                  );
-                }}
-                p="xs"
-                h="100%"
-                w="100%"
-                className="group"
-              >
-                <div className="flex-col-start">
-                  <div className="w-full gap-x-2 flex-row-between">
-                    <Title order={4}>{o.title}</Title>
-                    <HiArrowSmRight className="mt-5 hidden group-hover:inline-block" />
-                  </div>
-                  <Text size="lg" color="dimmed" ta="left">
-                    {o.description}
-                  </Text>
-                </div>
-              </Card>
-            </div>
-          ))}
-        </div>
-      </div>
-      <div className="space-y-10 pb-10">
-        <div>
-          <Title ta="center" order={3} mb={24}>
-            Tone Of Voice Examples
-          </Title>
-          <Spoiler
-            maxHeight={80}
-            showLabel="Show more"
-            hideLabel="Hide"
-            styles={{ control: { width: "100%" } }}
-          >
-            <div className="grid grid-cols-3 gap-x-3 gap-y-5 sm:grid-cols-5 md:grid-cols-6">
-              {tones.map((o, i) => (
-                <Badge key={i} color="gray" size="lg">
-                  <Text color="dimmed" tt="none">
-                    {o.name}
-                  </Text>
-                </Badge>
-              ))}
-            </div>
-          </Spoiler>
-        </div>
-        <div>
-          <Title ta="center" order={3} mb={24}>
-            Verbs Examples
-          </Title>
-          <Spoiler
-            maxHeight={80}
-            showLabel="Show more"
-            hideLabel="Hide"
-            styles={{ control: { width: "100%" } }}
-          >
-            <div className="grid grid-cols-3 gap-x-3 gap-y-5 sm:grid-cols-5 md:grid-cols-6">
-              {verbs.map((o, i) => (
-                <Badge color="gray" key={i} size="lg">
-                  <Text color="dimmed" tt="none">
-                    {o.name}
-                  </Text>
-                </Badge>
-              ))}
-            </div>
-          </Spoiler>
-        </div>
-      </div>
-    </>
-  );
-};
 
 //======================================
 export const Markdown = ({ content }: { content: string }) => {
@@ -375,7 +235,7 @@ export const Chats = () => {
   ) as ConversationT;
   const thread = conversation?.thread || [];
 
-  if (thread.length < 1 && status !== "loading") return <PromptTips />;
+  if (thread.length < 1 && status !== "loading") return <InitialChatsView />;
 
   return (
     <section className="w-full pb-5 pt-4">
