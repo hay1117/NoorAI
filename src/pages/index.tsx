10a2781a7ee854488d7838959d0e9dede4d3754c
hay1117 <kurainakamura047@gmail.com>
2023-04-27 09:37:14 +0200

    feat: add layout

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,10 +1,14 @@
-import { Button } from "@mantine/core";
+import { AppShell, Button } from "@mantine/core";
 import { type NextPage } from "next";
 import Head from "next/head";
 import Link from "next/link";
 import { useStore } from "@/hooks";
+import { Footer, Header } from "@/components";
+import * as React from "react";
 const Home: NextPage = () => {
   const conversations = useStore((state) => state.conversations);
+  const [opened, setOpened] = React.useState(false);
+
   return (
     <>
       <Head>
@@ -12,18 +16,23 @@ const Home: NextPage = () => {
         <meta name="description" content="NoorAI" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main className="grid min-h-screen place-items-center">
-        <div className="flex-col-center">
-          <h1>Experimental Project</h1>
-          <Link
-            href={
-              conversations?.[0]?.id ? `/${conversations?.[0].id}` : "/chat"
-            }
-          >
-            <Button>Start chating</Button>
-          </Link>
-        </div>
-      </main>
+      <AppShell
+        footer={<Footer />}
+        header={<Header opened={opened} setOpened={setOpened} />}
+      >
+        <main className="grid min-h-screen place-items-center">
+          <div className="pb-32 flex-col-center">
+            {/* <h1 className="font-extrabold text-5xl">Intuitive UI to ChatGPT</h1> */}
+            <Link
+              href={
+                conversations?.[0]?.id ? `/${conversations?.[0].id}` : "/chat"
+              }
+            >
+              <Button size="lg">Start chating</Button>
+            </Link>
+          </div>
+        </main>
+      </AppShell>
     </>
   );
 };
