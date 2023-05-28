83563da326eaf9a2a143d4718c15d534a094a8f4
hay1117 <kurainakamura047@gmail.com>
2023-05-28 11:38:48 +0200

    feat: tweak hero section

--- src/components/Hero/Hero.tsx
+++ src/components/Hero/Hero.tsx
@@ -1,5 +1,4 @@
 import { useStore } from "@/hooks";
-import { Button } from "@mantine/core";
 import Link from "next/link";
 import style from "./Hero.module.css";
 
@@ -23,36 +22,52 @@ const GridBG = () => (
     <svg x="50%" y="-1" className="overflow-visible fill-gray-800/20">
       <path
         d="M-200 0h201v201h-201Z M600 0h201v201h-201Z M-400 600h201v201h-201Z M200 800h201v201h-201Z"
-        stroke-width="0"
+        strokeWidth="0"
       />
     </svg>
     <rect
       width="100%"
       height="100%"
-      stroke-width="0"
+      strokeWidth="0"
       fill="url(#983e3e4c-de6d-4c3f-8d64-b9761d1534cc)"
     />
   </svg>
 );
+const content = {
+  cta: "Start Chatting",
+};
+
 //======================================
 export const Hero = () => {
   const conversations = useStore((state) => state.conversations);
   return (
     <>
       <GridBG />
-      <section
-        className={style.radial + " grid h-[90vh] grow place-items-center"}
-      >
-        <div className="pb-14 flex-col-center">
-          <h1 className="text-center font-extrabold uppercase">
-            Intuitive, Transparent, Powerfull
-          </h1>
+      <section className={" grid h-[90vh] grow place-items-center"}>
+        <div className="space-y-6 pb-14 flex-col-center">
+          <div>
+            <h1
+              className={
+                style.heroHeading +
+                " pb-2 text-center text-6xl font-extrabold uppercase tracking-tight"
+              }
+            >
+              Transparent, <br />
+              Intuitive, Powerfull
+            </h1>
+            <p className="text-center text-lg text-zinc-400">
+              (expirmental project)
+            </p>
+          </div>
           <Link
             href={
               conversations?.[0]?.id ? `/${conversations?.[0].id}` : "/chat"
             }
+            className="mx-auto"
           >
-            <Button size="lg">Start chating</Button>
+            <button className="mx-auto rounded bg-white/90 px-8 py-2 text-xl font-bold text-zinc-800 duration-200 hover:scale-105">
+              {content.cta}
+            </button>
           </Link>
         </div>
       </section>
--- src/pages/[chatId].tsx
+++ src/pages/[chatId].tsx
@@ -1,6 +1,12 @@
 import Head from "next/head";
 import clsx from "clsx";
-import { AppShell, Navbar, useMantineTheme, Paper } from "@mantine/core";
+import {
+  AppShell,
+  Navbar,
+  useMantineTheme,
+  Paper,
+  TypographyStylesProvider,
+} from "@mantine/core";
 import * as React from "react";
 import { useRouter } from "next/router";
 import { useStore } from "../hooks";
@@ -34,59 +40,61 @@ const ChatPage = (props: StoreCtxT) => {
       <Head>
         <title>{"Chat Page"}</title>
       </Head>
-      <FormProv {...methods}>
-        <StoreCtxProv value={props}>
-          <AppShell
-            padding={0}
-            navbar={
-              <Navbar
-                py={0}
-                px={6}
-                hiddenBreakpoint="sm"
-                hidden={!opened}
-                width={{ sm: 280, lg: 300 }}
-                bg={
-                  theme.colorScheme === "dark"
-                    ? theme.colors.dark[9]
-                    : theme.colors.gray[2]
-                }
-                color={
-                  theme.colorScheme === "dark"
-                    ? theme.colors.dark[2]
-                    : theme.colors.gray[6]
-                }
-                styles={{}}
-                withBorder={false}
-              >
-                <Sidebar />
-              </Navbar>
-            }
-            footer={undefined}
-            header={<Header opened={opened} setOpened={setOpened} />}
-          >
-            {/* Content */}
-            <div className="flex h-full w-full flex-col px-2 md:px-4">
-              <div className="mx-auto w-full max-w-3xl grow">
-                <React.Suspense fallback={<div>Loading...</div>}>
-                  <Chats />
-                </React.Suspense>
+      <TypographyStylesProvider>
+        <FormProv {...methods}>
+          <StoreCtxProv value={props}>
+            <AppShell
+              padding={0}
+              navbar={
+                <Navbar
+                  py={0}
+                  px={6}
+                  hiddenBreakpoint="sm"
+                  hidden={!opened}
+                  width={{ sm: 280, lg: 300 }}
+                  bg={
+                    theme.colorScheme === "dark"
+                      ? theme.colors.dark[9]
+                      : theme.colors.gray[2]
+                  }
+                  color={
+                    theme.colorScheme === "dark"
+                      ? theme.colors.dark[2]
+                      : theme.colors.gray[6]
+                  }
+                  styles={{}}
+                  withBorder={false}
+                >
+                  <Sidebar />
+                </Navbar>
+              }
+              footer={undefined}
+              header={<Header opened={opened} setOpened={setOpened} />}
+            >
+              {/* Content */}
+              <div className="flex h-full w-full flex-col px-2 md:px-4">
+                <div className="mx-auto w-full max-w-3xl grow">
+                  <React.Suspense fallback={<div>Loading...</div>}>
+                    <Chats />
+                  </React.Suspense>
+                </div>
+                <Paper
+                  p={0}
+                  radius={0}
+                  className={clsx(
+                    "sticky bottom-0 z-10 flex w-full items-start justify-center backdrop-blur-sm",
+                    theme.colorScheme === "dark"
+                      ? "bg-[#101113]/40"
+                      : `bg-[#E9ECEF]/30`
+                  )}
+                >
+                  <PromptArea />
+                </Paper>
               </div>
-              <Paper
-                p={0}
-                radius={0}
-                className={clsx(
-                  "sticky bottom-0 z-10 flex w-full items-start justify-center backdrop-blur-sm",
-                  theme.colorScheme === "dark"
-                    ? "bg-[#101113]/40"
-                    : `bg-[#E9ECEF]/30`
-                )}
-              >
-                <PromptArea />
-              </Paper>
-            </div>
-          </AppShell>
-        </StoreCtxProv>
-      </FormProv>
+            </AppShell>
+          </StoreCtxProv>
+        </FormProv>
+      </TypographyStylesProvider>
     </>
   );
 };
